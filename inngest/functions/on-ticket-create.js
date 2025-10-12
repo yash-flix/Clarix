d7927c9425113b8749e5d91f7d749e3fda59cd02
Yash Rane <yash.tushar13@gmail.com>
2025-10-12 07:36:04 -0700

    fixed some errors

--- inngest/functions/on-ticket-create.js
+++ inngest/functions/on-ticket-create.js
@@ -1,12 +1,12 @@
-import {inngest} from "../client"
+import {inngest} from "../client.js"
 import Ticket from "../../models/ticket.js"
 import { NonRetriableError } from "inngest";
-import { sendMail } from "../../utilis/mailer";
+import { sendMail } from "../../utilis/mailer.js";
 import { analyzeTicket } from "../../utilis/ai.js";
 import User from "../../models/user.js"
 
 
-export const onTicketCreated = inggest.createFunction(
+export const onTicketCreated = inngest.createFunction(
    {id:"on-ticket-created" , retries:2},
     {event:"ticket/created"},
     async ({event , step}) =>
@@ -45,10 +45,38 @@ export const onTicketCreated = inggest.createFunction(
         })
         const moderator = await step.run("assign-moderator" , async()=>
         {
+            const user = await User.findOne({
+                role:"moderator",
+                skills : {
+                    $elemMatch: { 
+                        $regex : relatedSkills.join("|") , $options:"i"
+                     }
+                }
+            });
+            if(!user)
+            {
+                user = await User.findOne({role:"admin"})
+            }
+
+            await Ticket.findByIdAndUpdate(ticketId._id , {assignedTo:user?._id || null })
+            return user;
+        });
+        await step.run("send-email-notification" , async()=>{
+            if(moderator)
+            {
+                 const finalTicket = await Ticket.findById(ticket._id);
+                await sendMail( moderator.email,
+                    "New Ticket Assigned",
+                    `A new ticket titled "${finalTicket.title}" has been assigned to you. Please review and take the necessary actions.`
+                )
+            }
 
         })
+        return {success:true};
 
         } catch (error) {
+            console.error("Error in onTicketCreated function:", error.message);
+            return {success:false}
             
         }
     }
