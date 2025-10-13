1fafd1bf8ba51bc81ef2989287f8685ecca557f1
Yash Rane <yash.tushar13@gmail.com>
2025-10-13 07:01:22 -0700

    implemented ticket route

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,5 +1,11 @@
 import express from "express"
+import{createTicket,getTicket,getTickets } from "../controllers/ticket.js"
+import { authenticate } from "../middlewares/auth.js"
 
 const router = express.Router();
+
+router.get("/", authenticate, getTickets);
+router.get(":/id" , authenticate , getTicket);
+router.post("/", authenticate , createTicket);
 
 export default router
