ad97ba10a3ab2b8a74140fd25c4cd80bab10df54
Yash Rane <yash.tushar13@gmail.com>
2025-10-17 12:18:12 -0700

    added routes

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import {serve} from "inngest/express"
 import userRoutes from "./routes/user.js"
+import ticketRoutes from "./routes/ticket.js"
+import {inngest} from "./inngest/client.js"
+import {onUserSignup} from "./inngest/functions/on-signup.js"
+import {onTicketCreated} from "./inngest/functions/on-ticket-create.js"
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -23,6 +28,11 @@ app.use(express.json());
 console.log("Mongo URI:", process.env.MONGO_URI); 
 
 app.use("/api/auth" , userRoutes)
+app.use("/api/tickets" , ticketRoutes);
+app.use("/api/inngest" ,serve({
+  client:inngest ,
+  functions:[onUserSignup , onTicketCreated]
+}) )
 
 
 
